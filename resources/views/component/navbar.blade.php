b81e5a16d4d05e4c78d39684f317a3f84758f79c
MyFirefly-creator <bdiwuputra@gmail.com>
2025-01-04 19:35:12 +0700

    first commit

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom shadow-sm">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">Moti-On</a>
+        <a class="navbar-brand" href="/">Stellar Memories</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -29,16 +29,23 @@
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
 
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle d-flex align-items-center" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="{{ asset('profil/' . Auth::user()->image) }}" alt="Profile" class="rounded-circle" width="30" height="30">
-                </button>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-                    <li><a class="dropdown-item" href="{{ route('sesi.profil.index') }}">Profile</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="{{ route('sesi.logout') }}">Logout</a></li>
-                </ul>
-            </div>
+            @if(Auth::check())
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle d-flex align-items-center" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="{{ asset('profil/' . Auth::user()->image) }}" alt="Profile" class="rounded-circle" width="30" height="30">
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                        <li><a class="dropdown-item" href="{{ route('sesi.profil.index') }}">Profile</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="{{ route('sesi.logout') }}">Logout</a></li>
+                    </ul>
+                </div>
+            @else
+                <div>
+                    <a href="{{ route('sesi.login') }}" class="btn btn-outline-primary me-2">Login</a>
+                    <a href="{{ route('sesi.register') }}" class="btn btn-primary">Register</a>
+                </div>
+            @endif
         </div>
     </div>
 </nav>
@@ -47,14 +54,12 @@
     .rounded-circle {
         object-fit: cover;
     }
-
     @media (max-width: 991px) {
         .navbar .form-control {
             width: 100%;
             margin-bottom: 10px;
         }
-
-        .navbar .dropdown {
+        .navbar .dropdown, .navbar div {
             margin-top: 10px;
         }
     }
